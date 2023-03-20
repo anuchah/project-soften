748dd838b970d03b4175539a67f7771b15642279
Mongkol-9001 <darkange140210@gmail.com>
2023-03-20 17:26:34 +0700

    Make dialog countdown embed in Jframe

--- src/main/java/nl/tudelft/jpacman/ui/DialogCountDown.java
+++ src/main/java/nl/tudelft/jpacman/ui/DialogCountDown.java
@@ -2,6 +2,8 @@ package nl.tudelft.jpacman.ui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.*;
@@ -28,6 +30,14 @@ public class DialogCountDown extends JDialog {
         label.setFont(new Font("Arial", Font.BOLD, 130));
         label.setForeground(Color.WHITE);
         add(label);
+
+        owner.addComponentListener(new ComponentAdapter() {
+            public void componentMoved(ComponentEvent e) {
+                int x = owner.getLocation().x + (owner.getWidth() - getWidth()) / 2;
+                int y = owner.getLocation().y + (owner.getHeight() - getHeight()) / 2;
+                setLocation(x, y);
+            }
+        });
         timer = new Timer(1000, new ActionListener() {
 
             @Override
