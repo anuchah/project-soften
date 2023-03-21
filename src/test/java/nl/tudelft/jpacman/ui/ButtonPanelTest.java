e1a02a0da02308e087177e1bee3f6ac5de4eb92d
Thanawat1125 <Thanawat@kkumail.com>
2023-03-21 16:11:16 +0700

    update: test ButtonPanel and WinPage

--- src/test/java/nl/tudelft/jpacman/ui/ButtonPanelTest.java
+++ src/test/java/nl/tudelft/jpacman/ui/ButtonPanelTest.java
@@ -1,36 +1,30 @@
 package nl.tudelft.jpacman.ui;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ButtonPanelTest {
+
     private Map<String, Action> buttons;
     private JFrame parent;
     private ButtonPanel buttonPanel;
 
     @BeforeEach
     void setUp() {
-        buttons = new HashMap<>();
+        buttons = Collections.singletonMap("Click me", () -> System.out.println("Button clicked!"));
         parent = new JFrame();
         buttonPanel = new ButtonPanel(buttons, parent);
     }
 
     @Test
     void testAddButton() {
-        Action action = new Action() {
-            @Override
-            public void doAction() {
-                System.out.println("Button clicked!");
-            }
-        };
-        buttons.put("Click me", action);
         buttonPanel.add(new JButton("Click me"));
-
-        Assertions.assertEquals(buttons.size(), buttonPanel.getComponentCount());
+        assertEquals(buttons.size(), buttonPanel.getComponentCount());
     }
 }
