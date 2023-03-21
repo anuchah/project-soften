24e75df6f26797f7bb01ced0382bc9b8f5906985
Thanawat1125 <Thanawat@kkumail.com>
2023-03-21 07:32:17 +0700

    update: test LostPage

--- src/test/java/nl/tudelft/jpacman/ui/LostPageTest.java
+++ src/test/java/nl/tudelft/jpacman/ui/LostPageTest.java
@@ -1,24 +1,72 @@
 package nl.tudelft.jpacman.ui;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import nl.tudelft.jpacman.ui.LostPage;
+import javax.swing.JPanel;
 
-public class LostPageTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-    public static void main(String[] args) {
-        LostPage lp = new LostPage();
-        JButton btn1 = new JButton("Restart");
-        JButton btn2 = new JButton("Quit");
-        lp.addButton("src/main/resources/button/restartbutton.png", btn1, 1, 0);
-        lp.addButton("src/main/resources/button/quitbutton.png", btn2, 1, 1);
-        lp.setScore(1000);
+class LostPageTest {
 
+    private LostPage lostPage;
+
+    @BeforeEach
+    void setUp() {
+        lostPage = new LostPage();
+    }
+
+    @Test
+    void testAddButton() {
+        JButton restartButton = new JButton("Restart");
+        JButton quitButton = new JButton("Quit");
+
+        lostPage.addButton("src/main/resources/button/restartbutton.png", restartButton, 1, 0);
+        lostPage.addButton("src/main/resources/button/quitbutton.png", quitButton, 1, 1);
+
+        JPanel buttonPanel = (JPanel) lostPage.getComponent(0);
+        assertNotNull(buttonPanel);
+        assertEquals(2, buttonPanel.getComponentCount());
+
+        JButton firstButton = (JButton) buttonPanel.getComponent(0);
+        assertNotNull(firstButton);
+        assertEquals(restartButton, firstButton);
+
+        JButton secondButton = (JButton) buttonPanel.getComponent(1);
+        assertNotNull(secondButton);
+        assertEquals(quitButton, secondButton);
+    }
+
+    @Test
+    void testSetScore() {
+        int score = 1000;
+        lostPage.setScore(score);
+
+        JPanel scorePanel = (JPanel) lostPage.getComponent(1);
+        assertNotNull(scorePanel);
+        assertEquals(1, scorePanel.getComponentCount());
+
+        String expectedLabelText = "Your score: " + score;
+        String actualLabelText = scorePanel.getComponent(0).getName();
+        assertEquals(expectedLabelText, actualLabelText);
+    }
+
+    @Test
+    void testFrame() {
         JFrame frame = new JFrame("Lost Page Test");
-        frame.add(lp);
+        frame.add(lostPage);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
         frame.setVisible(true);
-    }
 
+        assertEquals("Lost Page Test", frame.getTitle());
+        assertEquals(lostPage, frame.getContentPane().getComponent(0));
+        assertEquals(JFrame.EXIT_ON_CLOSE, frame.getDefaultCloseOperation());
+        assertEquals(800, frame.getWidth());
+        assertEquals(800, frame.getHeight());
+        assertEquals(true, frame.isVisible());
+    }
 }
